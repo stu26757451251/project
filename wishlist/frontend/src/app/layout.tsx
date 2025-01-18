88227a011a93e52ff4b7d05780c8e37a1aec5773
stu26757451251 <stu26757451251@gmail.com>
2025-01-18 20:42:45 +0800

    feat: set the font family to Poppins

--- wishlist/frontend/src/app/layout.tsx
+++ wishlist/frontend/src/app/layout.tsx
@@ -3,19 +3,25 @@ import { Metadata } from 'next'
 import './globals.css'
 import Content from '@/components/content'
 import { ReactNode } from 'react'
+import axios from 'axios'
+import { Poppins } from 'next/font/google'
+
+axios.defaults.baseURL = `http://${process.env.BACKEND_HOST}:${process.env.BACKEND_PORT}`
 
 export const metadata: Metadata = {
   icons: {
-    icon: './icon.png'
+    icon: '/cart.png'
   },
   title: 'Wishlist',
   description: 'This APP is for those people who want to write down their wishes.'
 }
 
+const poppins = Poppins({ subsets: ['latin'], weight: '400' })
+
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
-      <body className="h-screen">
+    <html lang="en" className={poppins.className}>
+      <body className="h-screen min-w-[400px] overflow-y-hidden">
         {/* The height of header-bar is 60px, thus the container should -60px */}
         <HeaderBar></HeaderBar>
         <div id="content" className="h-[calc(100%-60px)]">
