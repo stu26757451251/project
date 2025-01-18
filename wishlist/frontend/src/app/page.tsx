a0f81cb1cca9313fac18bf0e3709c630159811d4
stu26757451251 <stu26757451251@gmail.com>
2025-01-18 20:40:56 +0800

    refactor: remove unnecessary component and files

--- wishlist/frontend/src/app/page.tsx
+++ wishlist/frontend/src/app/page.tsx
@@ -1,12 +1,13 @@
-import MainContent from '@/components/main-content'
-import Title from '@/components/main-content/title'
-
 export default function Page() {
   return (
     <>
-      <div data-testid="wish-content" className="justify-self-center">
-        <Title title="Try To Go To Bed Before 12 O'Clock" />
-        <MainContent />
+      <div className="h-full flex items-center justify-center overflow-x-auto">
+        <div
+          data-testid="content"
+          className="mx-5 p-10 justify-items-center bg-white border rounded-md border-slate-600">
+          <div className="text-xl font-bold pb-3">Welcome Wishlist</div>
+          <div className="place-self-center">Record your todo list and life plans !</div>
+        </div>
       </div>
     </>
   )
--- wishlist/frontend/src/types/task.ts
+++
@@ -1,8 +0,0 @@
-type Emergency = 'Emergency' | 'Not Emergency'
-type Important = 'Important' | 'Not Important'
-
-export type Task = {
-  name: string
-  emergency: Emergency
-  important: Important
-}
