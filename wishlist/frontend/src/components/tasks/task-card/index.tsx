e4589b37baa1a38f673ad1c932f4fe5f3572b586
stu26757451251 <stu26757451251@gmail.com>
2025-01-18 20:44:41 +0800

    feat: create the TaskCard component

---
+++ wishlist/frontend/src/components/tasks/task-card/index.tsx
@@ -0,0 +1,44 @@
+import { Task } from '@/types/tasks/task'
+import dayjs from 'dayjs'
+import { FaClock } from 'react-icons/fa6'
+
+type TaskCardProps = {
+  dataTestId: string
+  task: Task
+}
+
+const formatDate = (date: Date): string => dayjs(date).format('YYYY/MM/DD')
+
+export default function TaskCard({ task, dataTestId }: TaskCardProps) {
+  const { name, dueDate, priority, emergency, important, frequency } = task
+  const hasTags = (task: Task): boolean =>
+    task.emergency !== undefined || task.important !== undefined || task.frequency !== undefined
+
+  return (
+    <div className="grid border rounded-xl px-4 gap-y-2 py-3" data-testid={dataTestId}>
+      <span className="block font-bold text-base">{name}</span>
+      {dueDate && <span className="block font-light text-sm">Due Date: {formatDate(dueDate)}</span>}
+      {priority && <span className="block font-light text-sm">Priority: {priority}</span>}
+      {hasTags(task) && (
+        <div className="flex">
+          {emergency && (
+            <span className="rounded p-[5px] text-xs mr-2 bg-green-200 text-green-900">
+              {emergency}
+            </span>
+          )}
+          {important && (
+            <span className="rounded p-[5px] text-xs mr-2 bg-red-200 text-red-900">
+              {important}
+            </span>
+          )}
+          {frequency && (
+            <span className="inline-flex items-center rounded p-[5px] text-xs mr-2 bg-slate-200 text-slate-900">
+              <FaClock size="14" className="mr-1 flex-shrink-0" />
+              {frequency}
+            </span>
+          )}
+        </div>
+      )}
+    </div>
+  )
+}
